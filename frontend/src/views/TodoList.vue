290dd7e26643967adafb2b3991b9dc94ef71f3e6
pep299 <eniarup@gmail.com>
2020-05-17 16:19:54 +0900

    入替仮実装 TODO:v-list → v-card化

--- frontend/src/views/TodoList.vue
+++ frontend/src/views/TodoList.vue
@@ -52,8 +52,11 @@
       <v-divider vertical></v-divider>
 
       <v-spacer></v-spacer>
-      <v-btn icon class="mx-4">
+      <v-btn v-if="!isSwap" icon class="mx-4" @click="startSwapTasks">
         <v-icon>mdi-swap-vertical</v-icon>
+      </v-btn>
+      <v-btn v-else icon class="mx-4" @click="endSwapTasks">
+        <v-icon color="green">mdi-check-bold</v-icon>
       </v-btn>
       <v-spacer></v-spacer>
     </v-row>
@@ -61,71 +64,89 @@
     <v-divider class="mb-4"></v-divider>
 
     <v-card v-if="tasks.length > 0">
-      <v-slide-y-transition>
+      <v-list v-if="isSwap" class="py-0">
+        <draggable v-model="editTasks">
+        <!-- <draggable v-model="editTasks" @end="draggableEnd"> -->
+          <template v-for="(task, i) in editTasks">
+            <!-- <v-divider
+              v-if="i !== 0"
+              :key="`${i}-divider`"
+            ></v-divider> -->
+
+            <v-list-item :key="`${i}-${task.text}`">
+              <v-icon>mdi-drag-vertical</v-icon>
+              <div
+                :class="task.done && 'grey--text' || 'primary--text'"
+                class="ml-4"
+                v-text="task.text"
+              ></div>
+            </v-list-item>
+          </template>
+        </draggable>
+      </v-list>
+      <template v-else>
         <v-list class="py-0">
-          <draggable v-model="tasks" >
-            <template v-for="(task, i) in tasks">
-              <v-divider
-                v-if="i !== 0"
-                :key="`${i}-divider`"
-              ></v-divider>
+          <template v-for="(task, i) in tasks">
+            <v-divider
+              v-if="i !== 0"
+              :key="`${i}-divider`"
+            ></v-divider>
 
-              <v-list-item :key="`${i}-${task.text}`">
-                <v-list-item-action>
-                  <v-checkbox
-                    v-model="task.done"
-                    :color="task.done && 'grey' || 'primary'"
-                  >
-                    <template
-                      v-if="i !== editTargetIndex"
-                      v-slot:label
-                    >
-                      <div
-                        :class="task.done && 'grey--text' || 'primary--text'"
-                        class="ml-4"
-                        v-text="task.text"
-                      ></div>
-                    </template>
-                  </v-checkbox>
-                </v-list-item-action>
-
-                <v-text-field
-                  v-if="i === editTargetIndex"
-                  v-model="editText"
-                  outlined
-                  dense
-                  hide-details
-                  class="ml-n5 mr-1"
+            <v-list-item :key="`${i}-${task.text}`">
+              <v-list-item-action>
+                <v-checkbox
+                  v-model="task.done"
+                  :color="task.done && 'grey' || 'primary'"
                 >
-                  <template v-slot:append>
-                    <v-icon @click="saveText(task)" color="green">mdi-check-bold</v-icon>
-                    <v-icon @click="endEditText" color="grey" class="ml-2">mdi-close</v-icon>
-                  </template>
-                </v-text-field>
-
-                <template v-else>
-                  <v-spacer></v-spacer>
-
-                <!-- <v-scroll-x-transition>
-                  <v-icon
-                    v-if="task.done"
-                    color="success"
+                  <template
+                    v-if="i !== editTargetIndex"
+                    v-slot:label
                   >
-                    check
-                  </v-icon>
-                </v-scroll-x-transition> -->
-                  <v-btn @click="startEditText(i, task.text)" icon>
-                    <v-icon>mdi-pencil</v-icon>
-                  </v-btn>
+                    <div
+                      :class="task.done && 'grey--text' || 'primary--text'"
+                      class="ml-4"
+                      v-text="task.text"
+                    ></div>
+                  </template>
+                </v-checkbox>
+              </v-list-item-action>
+
+              <v-text-field
+                v-if="i === editTargetIndex"
+                v-model="editText"
+                outlined
+                dense
+                hide-details
+                class="ml-n5 mr-1"
+              >
+                <template v-slot:append>
+                  <v-icon @click="saveText(task)" color="green">mdi-check-bold</v-icon>
+                  <v-icon @click="endEditText" color="grey" class="ml-2">mdi-close</v-icon>
                 </template>
-                <v-btn @click="deleteTask(task.order)" icon class="">
-                  <v-icon>mdi-trash-can-outline</v-icon>
+              </v-text-field>
+
+              <template v-else>
+                <v-spacer></v-spacer>
+
+              <!-- <v-scroll-x-transition>
+                <v-icon
+                  v-if="task.done"
+                  color="success"
+                >
+                  check
+                </v-icon>
+              </v-scroll-x-transition> -->
+                <v-btn @click="startEditText(i, task.text)" icon>
+                  <v-icon>mdi-pencil</v-icon>
                 </v-btn>
-              </v-list-item>
-            </template>
-          </draggable>
+              </template>
+              <v-btn @click="deleteTask(i)" icon class="">
+                <v-icon>mdi-trash-can-outline</v-icon>
+              </v-btn>
+            </v-list-item>
+          </template>
         </v-list>
-      </v-slide-y-transition>
+      </template>
     </v-card>
   </v-container>
 </template>
@@ -141,17 +162,14 @@ export default {
   data: () => ({
     tasks: [
       {
-        id: 1,
         done: false,
         text: 'Foobar'
       },
       {
-        id: 2,
         done: false,
         text: 'Fizzbuzz'
       },
       {
-        id: 3,
         done: true,
         text: 'bubble sort'
       },
@@ -177,17 +195,24 @@ export default {
   methods: {
     create() {
       this.tasks.push({
-        id: this.tasks.length + 1,
         done: false,
         text: this.task,
       })
       this.task = ""
     },
+    startSwapTasks() {
+      this.editTasks = JSON.parse(JSON.stringify(this.tasks))
+      this.isSwap = true
+    },
+    endSwapTasks() {
+      // this.save()
+      this.tasks = this.editTasks
+      this.isSwap = false
+    },
     startEditText(index, text) {
       this.isEdit = true
       this.editTargetIndex = index
       this.editText = text
-      console.log(index, text)
     },
     endEditText() {
       this.isEdit = false
@@ -202,6 +227,9 @@ export default {
     deleteTask(index) {
       this.tasks.splice(index, 1)
       this.endEditText()
+    },
+    draggableEnd(event) {
+      console.log(event)
     }
   }
 };
